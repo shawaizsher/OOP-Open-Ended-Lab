176777739a922aa01dcd0e4a9f9f869492e8e435
Hassan Ali <146338587+hassanzameer55@users.noreply.github.com>
2024-03-21 14:07:47 +0500

    Class course changes by hassan

--- main.cpp
+++ main.cpp
@@ -5,34 +5,21 @@
 #include<fstream>
 using namespace std;
 
+class Course;
+class Teacher;
+
 class Student
 {
 	private:
 		string studentID;
 		string name;
 		string email;
-		list<string> coursesEnrolled;
+		list<Course*> coursesEnrolled;
+		
 	public:
-		Stuednt(string t,string n,string e) : studentID(t), name(n) , email(e) {}
+		Student(string studentID, string name, string email): studentID(studentID), name(name), email(email) {}
 		
-		void enrolCouse(course)
-		{
-			
-		}
-		void dropCouse(course)
-		{
-			
-		}
 		
-		void dropCourse(course)
-		{
-			
-		}
-		
-		void view course(course)
-		{
-			
-		}
 		
 };
 
@@ -42,28 +29,59 @@ class Course
 	 private:
 	 	string courseCode;
 	 	string courseName;
-	 	Teacher * teacher;
-	 	list<string> studentsEnrolled;
+	 	Teacher* teacher;
+	 	
+	 	list<Student*> studentsEnrolled;
+	 	int maxCapacity;
+	 	
+	 	
 	 	
 	 public:
-	 
-	 void addStudent(student)
-	 {
 	 	
-	 }	
+	 	Course(string courseCode, string courseName, Teacher* teacher, int maxCapacity) : courseCode(courseCode), courseName(courseName), teacher(teacher), maxCapacity(maxCapacity) {}
 	 
-	 void removeStudent(student)
-	 {
-	 	
-	 }
+	 	void addStudent(Student* student)
+	 	{	
+	 		if (studentsEnrolled.size() < maxCapacity) 
+			{
+       			studentsEnrolled.push_back(student);
+        
+        		student->enrolCourse(this);
+        		cout << "Student " << student->getName() << " enrolled in course " << courseName << endl;
+    		} 
+			else
+			{
+        		cout << "Course " << courseName << " has reached its maximum capacity." << endl;
+    		}
+	 	}	
 	 
-	 void viewStudent(student)
-	 {
-	 	
-	 }
+	 	void removeStudent(Student* student)
+	 	{
+    		auto it = find(studentsEnrolled.begin(), studentsEnrolled.end(), student);
+    		if (it != studentsEnrolled.end()) 
+			{
+        		studentsEnrolled.erase(it);
+        
+        		student->dropCourse(this);
+        		cout << "Student " << student->getName() << " removed from course " << courseName << endl;
+    		} 
+			else 
+			{
+        		cout << "Student " << student->getName() << " is not enrolled in course " << courseName << endl;
+    		}
+	 	}
+	 
+	 	void viewStudents(Student* student)
+	 	{
+	 		cout << "Students enrolled in course " << courseName << ":" << endl;
+    
+    		for (auto student : studentsEnrolled) 
+			{
+        		cout << "Student ID: " << student->getStudentID() << ", Name: " << student->getName() << ", Email: " << student->getEmail() << endl;
+    		}
+		}
 };
 int main()
 {
      return 0;
 }
-

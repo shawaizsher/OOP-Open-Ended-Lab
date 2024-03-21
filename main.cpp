10557df0d6628c895e9cb2b7a36f7beecf11eae5
Shawaiz <145105002+shawaizsher@users.noreply.github.com>
2024-03-21 10:33:43 +0500

    updating code and flat file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include<iostream>
+#include<list>
+#include<vector>
+#include<string>
 using namespace std;
+class Student
+{
+   string studentID;
+   string name ;
+   string email ;
+   list<string>coursesEnrolled;
+   
+   // methods
+   public:
+   	void enrolCourse(course)
+   	{
+   		
+	   }
+	 void dropCourse(course)
+	 {
+	 	
+	   }  
+	void viewCourses(course)
+	{
+		
+	}
+   	
+};
 
+class Teacher
+{
+    
+};
+
+class Course
+{
+	 
+};
 int main()
 {
 	
